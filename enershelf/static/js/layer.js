9c081ad165386e67cd113152d7aa24172447515f
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2021-06-14 16:21:06 +0200

    Implemented multiple filters per layer

--- enershelf/static/js/layer.js
+++ enershelf/static/js/layer.js
@@ -63,12 +63,10 @@ function setDetailLayersOnDetailLayersSwitchClick(msg) {
 
 function filterChanged(msg, {layerForm}) {
   const layer_id = get_layer_id(layerForm);
-  const filter = get_layer_filter(layerForm);
+  const filters = get_layer_filters(layerForm);
   const layers = get_map_layer_ids(layer_id);
   $.each(layers, function (i, layer) {
-    if (filter != null) {
-      set_filter(layer, filter);
-    }
+    set_filters(layer, filters);
   })
   return logMessage(msg);
 }
@@ -112,13 +110,11 @@ function turn_off_layer(layer_form) {
 
 function turn_on_layer(layer_form) {
   const layer_id = get_layer_id(layer_form);
-  const filter = get_layer_filter(layer_form);
+  const filters = get_layer_filters(layer_form);
   const layers = get_map_layer_ids(layer_id);
   $.each(layers, function (i, layer) {
     map.setLayoutProperty(layer, "visibility", "visible");
-    if (filter != null) {
-      set_filter(layer, filter);
-    }
+    set_filters(layer, filters);
   })
   return layers;
 }
@@ -133,21 +129,30 @@ function get_map_layer_ids(layer_id) {
   return layers;
 }
 
-function get_layer_filter(layer_form) {
-  let filter = $(layer_form).find(".js-range-slider");
-  if (filter.length > 0) {
-    filter_name = filter[0].id.slice(3);
-    result = filter.data("ionRangeSlider").result;
-    return {
-      name: filter_name,
-      from: result.from,
-      to: result.to
-    }
-  }
+function get_layer_filters(layer_form) {
+  let filters = [];
+  let sliders = $(layer_form).find(".js-range-slider");
+  sliders.each(function (index, slider) {
+    filter_name = slider.id.slice(3);
+    result = $(slider).data("ionRangeSlider").result;
+    filters.push(
+      {
+        name: filter_name,
+        from: result.from,
+        to: result.to
+      }
+    )
+  });
+  return filters;
 }
 
-function set_filter(layer, filter) {
-  lower_bound = [">=", ["get", filter.name], filter.from];
-  upper_bound = ["<=", ["get", filter.name], filter.to];
-  map.setFilter(layer, ["all", lower_bound, upper_bound]);
+function set_filters(layer, filters) {
+  let map_filters = ["all"];
+  for (let i = 0; i < filters.length; i++) {
+    lower_bound = [">=", ["get", filters[i].name], filters[i].from];
+    upper_bound = ["<=", ["get", filters[i].name], filters[i].to];
+    map_filters.push(lower_bound);
+    map_filters.push(upper_bound);
+  }
+  map.setFilter(layer, map_filters);
 }
