e14ab08481bf6e0dff39210c428e52c3e8484836
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2021-09-07 10:20:10 +0200

    Added region-aware models

--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
 
-.PHONY : load_data empty_data dump_fixtures load_fixtures distill
+.PHONY : load_regions load_data empty_data dump_fixtures load_fixtures distill
 
 DISTILL=True
 export
 
+load_regions:
+	python manage.py shell --command="from egon.utils import data_processing; data_processing.load_regions()"
+
 load_data:
 	python manage.py shell --command="from enershelf.utils import data_processing; data_processing.load_data()"
+
+empty_regions:
+	python manage.py shell --command="from egon.utils import data_processing; data_processing.empty_data(models=data_processing.REGIONS)"
 
 empty_data:
 	python manage.py shell --command="from enershelf.utils import data_processing; data_processing.empty_data()"
--- enershelf/utils/data_processing.py
+++ enershelf/utils/data_processing.py
@@ -4,6 +4,7 @@ from config.settings.base import ROOT_DIR
 from enershelf.utils.ogr_layer_mapping import RelatedModelLayerMapping
 from enershelf.map.models import Region, District, Hospitals, HospitalsSimulated, Cluster, Nightlight
 
+REGIONS = []
 
 MODELS = [
     Region,
@@ -15,16 +16,33 @@ MODELS = [
 ]
 
 
+def load_regions(regions=None, verbose=True):
+    regions = regions or REGIONS
+    for region in regions:
+        print(f"Upload data for region '{region.__name__}'")
+        if hasattr(region, "data_folder"):
+            data_path = os.path.join(ROOT_DIR, "egon", "data", region.data_folder, f"{region.data_file}.gpkg")
+        else:
+            data_path = os.path.join(ROOT_DIR, "egon", "data", f"{region.data_file}.gpkg")
+        region_model = Region(layer_type=region.__name__.lower())
+        region_model.save()
+        instance = RelatedModelLayerMapping(
+            model=region, data=data_path, mapping=region.mapping, layer=region.layer, transform=4326,
+        )
+        instance.region = region_model
+        instance.save(strict=True, verbose=verbose)
+
+
 def load_data(models=None, verbose=True):
     models = models or MODELS
     for model in models:
         print(f"Upload data for model '{model.__name__}'")
+        if hasattr(model, "data_folder"):
+            data_path = os.path.join(ROOT_DIR, "enershelf", "data", model.data_folder, f"{model.data_file}.gpkg")
+        else:
+            data_path = os.path.join(ROOT_DIR, "enershelf", "data", f"{model.data_file}.gpkg")
         instance = RelatedModelLayerMapping(
-            model=model,
-            data=os.path.join(ROOT_DIR, "enershelf", "data", f"{model.data_file}.gpkg",),
-            mapping=model.mapping,
-            layer=model.layer,
-            transform=4326,
+            model=model, data=data_path, mapping=model.mapping, layer=model.layer, transform=4326,
         )
         instance.save(strict=True, verbose=verbose)
 
