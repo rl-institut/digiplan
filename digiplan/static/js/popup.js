212421b42fe52ad4c2e3f8a0cbb62043eb750560
Alexis Michaltsis <a.michaltsis@gmail.com>
2022-09-26 21:05:47 +0200

    Add basic placeholder chart

--- digiplan/static/js/popup.js
+++ digiplan/static/js/popup.js
@@ -1,10 +1,12 @@
 "use strict";
 
-function add_popup(layer_id, fields, template_id="default") {
+function add_popup(layer_id, fields, template_id = "default") {
   map.on("click", layer_id, function (e) {
     let coordinates;
     // Check if popup already exists:
-    if ($('.mapboxgl-popup').length > 0) {return;}
+    if ($('.mapboxgl-popup').length > 0) {
+      return;
+    }
 
     if ("lat" in e.features[0].properties) {
       // Get coordinates from lat/lon:
@@ -13,21 +15,68 @@ function add_popup(layer_id, fields, template_id="default") {
       // Get coordinates from geometry:
       coordinates = e.lngLat;
     }
-    var template = $($("#" + template_id + "_popup").prop("content")).find("div");
-    var clone = template.clone();
-    var table = clone.find("table");
-    for (var label in fields) {
+    const template = document.getElementById(template_id + "_popup");
+    const clone = template.content.cloneNode(true);
+    const html = clone.getElementById("popup_div");
+    const table = html.querySelector("table");
+    let tableRows = "";
+    for (const label in fields) {
       const key = fields[label];
       const value = e.features[0].properties[key];
-      let row = $("<tr>");
-      row.append($("<td>").text(label));
-      row.append($("<td>").text(value));
-      table.append(row);
+      tableRows += `<tr><td>${label}</td><td>${value}</td></tr>`;
     }
-    new maplibregl.Popup().setLngLat(coordinates).setHTML(clone.html()).addTo(map);
+    table.innerHTML = tableRows;
+
+    const chartDom = html.querySelector("#popup_chart");
+    const myChart = echarts.init(chartDom, null, {renderer: 'svg'});
+    let option;
+
+    option = {
+      animation: false,
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow'
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: ["2023", "2030", "2045"],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: 'Direct',
+          type: 'bar',
+          barWidth: '60%',
+          data: [20000, 22000, 24000]
+        }
+      ]
+    };
+
+    option && myChart.setOption(option);
+
+    requestAnimationFrame(() => {
+      new maplibregl.Popup().setLngLat(coordinates).setHTML(html.innerHTML).addTo(map);
+    });
   });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   $('#js-intro-modal').modal('show');
 });
