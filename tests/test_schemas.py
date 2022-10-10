ee1e0d136c3bee92c1543a6ea41b7aeb2153ae1f
Alexis Michaltsis <a.michaltsis@gmail.com>
2022-10-10 19:59:23 +0200

    Mock schema validation test by using schema store

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -35,9 +35,17 @@ def test_if_popup_default_example_loads():
 
 
 def test_if_popup_default_example_validates():
-    import jsonschema
+    from jsonschema import Draft202012Validator, RefResolver
 
+    from config.schemas import ATOM_CHART_SCHEMA  # noqa: I001
     from config.schemas import POPUP_DEFAULT_SCHEMA  # noqa: I001
     from config.schemas import POPUP_DEFAULT_EXAMPLE  # noqa: I001
 
-    assert jsonschema.validate(POPUP_DEFAULT_EXAMPLE, POPUP_DEFAULT_SCHEMA) is None  # noqa: S101
+    schema_store = {
+        ATOM_CHART_SCHEMA["$id"]: ATOM_CHART_SCHEMA,
+        POPUP_DEFAULT_SCHEMA["$id"]: POPUP_DEFAULT_SCHEMA,
+    }
+    resolver = RefResolver.from_schema(ATOM_CHART_SCHEMA, store=schema_store)
+    validator = Draft202012Validator(POPUP_DEFAULT_SCHEMA, resolver=resolver)
+
+    assert validator.validate(POPUP_DEFAULT_EXAMPLE) is None  # noqa: S101
