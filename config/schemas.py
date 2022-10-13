e99167a54b8a0d0ee8da0a7b5731100d32334fc0
Alexis Michaltsis <a.michaltsis@gmail.com>
2022-10-13 22:07:11 +0200

    Extract components and radically simplify existing schemas

--- config/schemas.py
+++ config/schemas.py
@@ -3,20 +3,23 @@ import os
 
 from config.settings.base import APPS_DIR
 
-SCHEMA_DIR = APPS_DIR.path("schemas")
+SCHEMAS_DIR = APPS_DIR.path("schemas")
+COMPONENTS_DIR = SCHEMAS_DIR.path("components")
 
-# ATOMS
-with open(os.path.join(SCHEMA_DIR, "atom.chart.schema.json"), "rb") as f:
-    ATOM_CHART_SCHEMA = json.loads(f.read())
+with open(os.path.join(SCHEMAS_DIR, "popup.schema.json"), "rb") as f:
+    POPUP_SCHEMA = json.loads(f.read())
 
-# ATOM EXAMPLES
-with open(os.path.join(SCHEMA_DIR, "atom.chart.example.json"), "rb") as f:
-    ATOM_CHART_EXAMPLE = json.loads(f.read())
+with open(os.path.join(SCHEMAS_DIR, "popup.example.json"), "rb") as f:
+    POPUP_EXAMPLE = json.loads(f.read())
 
-# SCHEMAS
-with open(os.path.join(SCHEMA_DIR, "popup.default.schema.json"), "rb") as f:
-    POPUP_DEFAULT_SCHEMA = json.loads(f.read())
+with open(os.path.join(COMPONENTS_DIR, "chart.schema.json"), "rb") as f:
+    CHART_SCHEMA = json.loads(f.read())
 
-# SCHEMA EXAMPLES
-with open(os.path.join(SCHEMA_DIR, "popup.default.example.json"), "rb") as f:
-    POPUP_DEFAULT_EXAMPLE = json.loads(f.read())
+with open(os.path.join(COMPONENTS_DIR, "chart.example.json"), "rb") as f:
+    CHART_EXAMPLE = json.loads(f.read())
+
+with open(os.path.join(COMPONENTS_DIR, "sources.schema.json"), "rb") as f:
+    SOURCES_SCHEMA = json.loads(f.read())
+
+with open(os.path.join(COMPONENTS_DIR, "sources.example.json"), "rb") as f:
+    SOURCES_EXAMPLE = json.loads(f.read())
