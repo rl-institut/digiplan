6a47a4eb9b3fb6160080dcbdb83419717a9d8e18
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-02-10 10:03:08 +0100

    Automatic init of visualization related calculations

--- digiplan/map/results/__init__.py
+++ digiplan/map/results/__init__.py
@@ -1,11 +1,9 @@
 from django_oemof import results
-from oemoflex.postprocessing import core as results_core
-from oemoflex.postprocessing import postprocessing
 
 from . import core, visualizations
 
-results.register_calculation(
-    results_core.ParametrizedCalculation(postprocessing.AggregatedFlows, {"to_nodes": ["demand"]})
-)
 core.add_visualization(visualizations.TotalCosts)
 core.add_visualization(visualizations.Demand)
+
+for visualization in core.VISUALIZATIONS.values():
+    results.register_calculation(visualization.calculation)
