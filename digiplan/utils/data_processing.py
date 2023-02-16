d0c7811d12c54f16c3720ad4499da0e70ba33096
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-02-16 14:32:29 +0100

    Removed rasters from data_processing.py

--- digiplan/utils/data_processing.py
+++ digiplan/utils/data_processing.py
@@ -2,11 +2,9 @@ import json
 import os
 
 from geojson import Feature, FeatureCollection, Point
-from raster.models import Legend
-from raster.models import RasterLayer as RasterModel
 
 from config.settings.base import DATA_DIR
-from digiplan.map.config.config import CLUSTER_GEOJSON_FILE, LAYER_STYLES, ZOOM_LEVELS
+from digiplan.map.config.config import CLUSTER_GEOJSON_FILE, ZOOM_LEVELS
 from digiplan.map.layers import LAYERS_DEFINITION, VectorLayerData
 from digiplan.map.models import (
     Biomass,
@@ -70,27 +68,6 @@ def load_data(models=None):
         instance.save(strict=True)
 
 
-def load_raster(layers=None):
-    layers = layers or LAYERS_DEFINITION
-    for layer in layers:
-        if not issubclass(layer.model, RasterModel):
-            continue
-        if RasterModel.objects.filter(name=layer.source).exists():
-            print(f"Skipping data for raster '{layer.name}' - Please empty raster first if you want to update data.")
-            continue
-        print(f"Upload data for raster '{layer.name}'")
-        rm = RasterModel(name=layer.source, rasterfile=layer.filepath)
-        rm.save()
-        if Legend.objects.filter(title=layer.legend).exists():
-            print(
-                f"Skipping legend '{layer.legend}' for raster '{layer.name}' - "
-                f"Please remove raster legend first if you want to update it."
-            )
-            continue
-        legend = Legend(title=layer.legend, json=json.dumps(LAYER_STYLES[layer.legend]))
-        legend.save()
-
-
 def build_cluster_geojson(cluster_layers: list[VectorLayerData] = None):
     cluster_layers = cluster_layers or LAYERS_DEFINITION
     features = []
@@ -116,12 +93,3 @@ def empty_data(models=None):
     models = models or MODELS
     for model in models:
         model.objects.all().delete()
-
-
-def empty_raster(layers=None):
-    layers = layers or LAYERS_DEFINITION
-    for layer in layers:
-        if not issubclass(layer.model, RasterModel):
-            continue
-        RasterModel.objects.filter(name=layer.source).delete()
-        Legend.objects.filter(title=layer.legend).delete()
