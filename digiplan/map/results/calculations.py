e04d06f08eea22b49032e22e988d236b38e297ff
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-03-01 11:52:00 +0100

    Fixed chart for installed_ee lookup

--- digiplan/map/results/calculations.py
+++ digiplan/map/results/calculations.py
@@ -1,13 +1,37 @@
 import json
-from typing import Optional
+from collections import namedtuple
+from typing import Dict, Optional
 
+import jsonschema
 from django.db.models import Sum
 
+from config import schemas
 from digiplan.map import models
 from digiplan.map.config import config
 
+LookupFunctions = namedtuple("PopupData", ("data_fct", "chart_fct"))
 
-def calc_installed_ee(municipality_id: Optional[int] = None):
+
+def create_chart(lookup):
+    with open(config.POPUPS_DIR.path(f"{lookup}_chart.json"), "r", encoding="utf-8") as chart_json:
+        chart = json.load(chart_json)
+    chart = LOOKUPS[lookup].chart_fct(chart)
+    jsonschema.validate(chart, schemas.CHART_SCHEMA)
+    return chart
+
+
+def create_data(lookup, mun_id):
+    with open(config.POPUPS_DIR.path(f"{lookup}.json"), "r", encoding="utf-8") as data_json:
+        data = json.load(data_json)
+
+    data["id"] = mun_id
+    data["data"]["region_value"] = LOOKUPS[lookup].data_fct()
+    data["data"]["municipality_value"] = LOOKUPS[lookup].data_fct(mun_id)
+
+    return data
+
+
+def get_data_for_installed_ee(municipality_id: Optional[int] = None):
     installed_ee = 0.0
     for renewable in models.RENEWABLES:
         if municipality_id:
@@ -21,21 +45,11 @@ def calc_installed_ee(municipality_id: Optional[int] = None):
     return installed_ee
 
 
-def create_chart(lookup):
-    with open(config.POPUPS_DIR.path(lookup + "_chart.json"), "r", encoding="utf-8") as jsonFile:
-        chart = json.load(jsonFile)
+def get_chart_for_installed_ee(chart):
+    chart["series"][0]["data"] = [{"key": 2023, "value": 2}, {"key": 2045, "value": 3}, {"key": 2050, "value": 4}]
     return chart
 
 
-def create_data(lookup, mun_id):
-    with open(config.POPUPS_DIR.path(lookup + ".json"), "r", encoding="utf-8") as jsonFile:
-        data = json.load(jsonFile)
-
-    data["id"] = mun_id
-    data["data"]["region_value"] = CALCULATIONS[lookup]()
-    data["data"]["municipality_value"] = CALCULATIONS[lookup](mun_id)
-
-    return data
-
-
-CALCULATIONS = {"installed_ee": calc_installed_ee}
+LOOKUPS: Dict[str, LookupFunctions] = {
+    "installed_ee": LookupFunctions(get_data_for_installed_ee, get_chart_for_installed_ee)
+}
