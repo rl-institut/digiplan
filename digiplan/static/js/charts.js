2edba035668f5aad0bbf26dc9e1f87fd5e5c699f
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-03-21 14:00:27 +0100

    Add electrictiy and mobility charts (static)

--- digiplan/static/js/charts.js
+++ digiplan/static/js/charts.js
@@ -4,6 +4,10 @@ const chart4Element = document.getElementById("chart4");
 const chart4 = echarts.init(chart4Element);
 const detailed_overview_chart = echarts.init(document.getElementById("detailed_overview_chart"));
 const ghg_overview_chart = echarts.init(document.getElementById("ghg_overview_chart"));
+const electricity_overview_chart = echarts.init(document.getElementById("electricity_overview_chart"));
+const electricity_THG_chart = echarts.init(document.getElementById("electricity_THG_chart"));
+const mobility_overview_chart = echarts.init(document.getElementById("mobility_overview_chart"));
+const mobility_THG_chart = echarts.init(document.getElementById("mobility_THG_chart"));
 
 const option = {
   title: {
@@ -314,17 +318,472 @@ const ghg_overview_option = {
   ]
 };
 
+const electricity_overview = {
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      // Use axis to trigger tooltip
+      type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
+    }
+  },
+  legend: {
+    show: true,
+    bottom: '15',
+    itemWidth: 14,
+    itemHeight: 14
+  },
+  grid: {
+    top: '10%',
+    left: '3%',
+    right: '25%',
+    bottom: '18%',
+    containLabel: true
+  },
+  xAxis: {
+    type: 'value',
+    show: true,
+    position: 'bottom',
+    name: 'TWh',
+      nameLocation: 'end',
+      nameTextStyle: 'Roboto',
+        width: '76',
+        heigth: '32',
+      fontWeight: '300',
+      fontSize: '14'
+  },
+  yAxis: {
+    type: 'category',
+    data: ['Bedarf',
+    'Ziel',
+    'Mein Szeanrio',
+    'Status Quo' ],
+    axisTick: {
+      show: false
+    }
+  },
+  series: [
+    {
+      name: 'Wind',
+      type: 'bar',
+      barWidth: '16',
+      stack: 'total',
+      color: '#1F82C0',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [0, 502, 400, 334]
+    },
+    {
+      name: 'Freiflächen - PV',
+      type: 'bar',
+      stack: 'total',
+      color: '#F6B93B',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [0, 382, 300, 234 ]
+    },
+    {
+      name: 'Aufdach - PV',
+      type: 'bar',
+      stack: 'total',
+      color: '#FFD660',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [0, 312, 280 , 254]
+    },
+        {
+      name: 'Bioenergie',
+      type: 'bar',
+      stack: 'total',
+      color: '#98D47E',
+      label: {
+        show: false
+      },
+      emphasis: {
+      },
+      data: [0, 136, 135, 134]
+    },
+    {
+      name: 'Konventionell',
+      type: 'bar',
+      stack: 'total',
+      color: '#1A1A1A',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [0 , 132, 200, 534 ]
+    },
+    {
+      name: 'GHG',
+      type: 'bar',
+      stack: 'total',
+      color: '#F5F5DC',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [400, 0 , 0, 0]
+    },
+    {
+      name: 'Haushalte',
+      type: 'bar',
+      stack: 'total',
+      color: '#A8DADC',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [360, 0 , 0, 0]
+    },
+     {
+      name: 'Industrie',
+      type: 'bar',
+      stack: 'total',
+      color: '#C27BA0',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [300, 0 , 0, 0]
+    },
+    {
+      name: 'Sonstiges',
+      type: 'bar',
+      stack: 'total',
+      color: '#B0BEC5',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [350, 0 , 0, 0]
+    },
+  ]
+};
+
+const electricity_THG = {
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      // Use axis to trigger tooltip
+      type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
+    }
+  },
+  legend: {
+    show: true,
+    bottom: '15',
+    itemWidth: 14,
+    itemHeight: 14
+  },
+  grid: {
+    top: '10%',
+    left: '3%',
+    right: '25%',
+    bottom: '18%',
+    containLabel: true
+  },
+  xAxis: {
+    type: 'value',
+    show: true,
+    position: 'bottom',
+    name: 'Mt CO₂-\nEmissionen',
+      nameLocation: 'end',
+      nameTextStyle: 'Roboto',
+        width: '76',
+        heigth: '32',
+      fontWeight: '300',
+      fontSize: '14'
+  },
+  yAxis: {
+    type: 'category',
+    data: ['Ziel',
+    'Mein Szenario',
+    'Status Quo',
+    '1990' ],
+    axisTick: {
+      show: false
+    }
+  },
+  series: [
+    {
+      name: 'GHG',
+      type: 'bar',
+      barWidth: '16',
+      stack: 'total',
+      color: '#F5F5DC',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [10, 30, 34, 37]
+    },
+    {
+      name: 'Haushalte',
+      type: 'bar',
+      stack: 'total',
+      color: '#A8DADC',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [20, 25 , 27, 30]
+    },
+     {
+      name: 'Industrie',
+      type: 'bar',
+      stack: 'total',
+      color: '#C27BA0',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [30, 40, 50, 60]
+    },
+    {
+      name: 'Sonstiges',
+      type: 'bar',
+      stack: 'total',
+      color: '#B0BEC5',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [30, 40 , 45, 50]
+    },
+  ]
+};
+
+const mobility_overview = {
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      // Use axis to trigger tooltip
+      type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
+    }
+  },
+  legend: {
+    show: true,
+    bottom: '15',
+    itemWidth: 14,
+    itemHeight: 14
+  },
+  grid: {
+    top: '10%',
+    left: '3%',
+    right: '25%',
+    bottom: '18%',
+    containLabel: true
+  },
+  xAxis: {
+    type: 'value',
+    show: true,
+    position: 'bottom',
+    name: 'Anzahl Autos',
+      nameLocation: 'end',
+      nameTextStyle: 'Roboto',
+        width: '76',
+        heigth: '32',
+      fontWeight: '300',
+      fontSize: '14'
+  },
+  yAxis: {
+    type: 'category',
+    data: ['Ziel\nSzenario',
+    'Mein \nSzenario',
+    'Status Quo'
+    ],
+    axisTick: {
+      show: false
+    }
+  },
+  series: [
+    {
+      name: 'Diesel',
+      type: 'bar',
+      barWidth: '16',
+      stack: 'total',
+      color: '#647078',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [50, 222, 400]
+    },
+    {
+      name: 'Benzin',
+      type: 'bar',
+      stack: 'total',
+      color: '#866E18',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [20, 182, 350]
+    },
+    {
+      name: 'Hybrid',
+      type: 'bar',
+      stack: 'total',
+      color: '#8FDCE1',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [100, 100, 140]
+    },
+        {
+      name: 'E-Auto',
+      type: 'bar',
+      stack: 'total',
+      color: '#98D47E',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [500, 300, 100]
+    },
+  ]
+};
+
+const mobility_THG = {
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      // Use axis to trigger tooltip
+      type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
+    }
+  },
+  legend: {
+    show: true,
+    bottom: '15',
+    itemWidth: 14,
+    itemHeight: 14
+  },
+  grid: {
+    top: '10%',
+    left: '3%',
+    right: '25%',
+    bottom: '18%',
+    containLabel: true
+  },
+  xAxis: {
+    type: 'value',
+    show: true,
+    position: 'bottom',
+    name: 'Mt CO₂-\nEmissionen',
+      nameLocation: 'end',
+      nameTextStyle: 'Roboto',
+        width: '76',
+        heigth: '32',
+      fontWeight: '300',
+      fontSize: '14'
+  },
+  yAxis: {
+    type: 'category',
+    data: ['Ziel',
+    'Mein Szenario',
+    'Status Quo',
+    '1990' ],
+    axisTick: {
+      show: false
+    }
+  },
+  series: [
+    {
+      name: 'Sockel',
+      type: 'bar',
+      barWidth: '16',
+      stack: 'total',
+      color: '#C8D8E4',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [30, 30, 30, 30]
+    },
+    {
+      name: 'Konventionell',
+      type: 'bar',
+      stack: 'total',
+      color: '#647078',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [15, 25 , 65, 100]
+    },
+     {
+      name: 'Erneuerbare Energien',
+      type: 'bar',
+      stack: 'total',
+      color: '#A8E7BA',
+      label: {
+        show: false
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [65, 65, 38, 10]
+    },
+  ]
+};
+
 function isVisible(element) {
   if (element.offsetParent) return true;
   return false;
 }
 
-function resizeChart() {
+function resizeCharts() {
   setTimeout(function () {
     if (isVisible(chart3Element)) chart3.resize();
     if (isVisible(chart4Element)) chart4.resize();
     detailed_overview_chart.resize();
     ghg_overview_chart.resize();
+    electricity_overview_chart.resize();
+    electricity_THG_chart.resize();
+    mobility_overview_chart.resize();
+    mobility_THG_chart.resize();
   }, 200);
 }
 
@@ -332,6 +791,10 @@ chart3.setOption(option);
 chart4.setOption(option);
 detailed_overview_chart.setOption(detailed_overview_option);
 ghg_overview_chart.setOption(ghg_overview_option);
+electricity_overview_chart.setOption(electricity_overview);
+electricity_THG_chart.setOption(electricity_THG);
+mobility_overview_chart.setOption(mobility_overview);
+mobility_THG_chart.setOption(mobility_THG);
 
-window.addEventListener("resize", resizeChart);
-document.addEventListener("show.bs.tab", resizeChart);
+window.addEventListener("resize", resizeCharts);
+document.addEventListener("show.bs.tab", resizeCharts);
