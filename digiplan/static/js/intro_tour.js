e1eafd62eb4de2d1f26a523586a2341d0f3b114e
Finn Hees <finn@hees-web.de>
2023-03-22 13:11:16 +0100

    add shepherd.js for tour creation 186

---
+++ digiplan/static/js/intro_tour.js
@@ -0,0 +1,83 @@
+const tour = new Shepherd.Tour({  // jshint ignore:line
+    useModalOverlay: true,
+    defaultStepOptions: {
+        cancelIcon: {
+            enabled: true
+        },
+        classes: 'class-1 class-2',
+        scrollTo: {behavior: 'smooth', block: 'center'}
+    }
+});
+
+
+tour.addStep({
+    title: 'Navigation',
+    text: 'Schritt für Schritt zu Ihrem eigenen Szenario',
+    attachTo: {
+        element: '.steps',
+        on: 'bottom'
+    },
+    buttons: [
+        {
+            action() {
+                return this.next();
+            },
+            text: 'Weiter'
+        }
+    ],
+    id: 'creating'
+});
+
+tour.addStep({
+    title: 'Einstellungen',
+    text: 'Schauen Sie sich die Situation heute an.',
+    attachTo: {
+        element: '#panel_1_today',
+        on: 'right'
+    },
+    buttons: [
+        {
+            action() {
+                return this.back();
+            },
+            classes: 'shepherd-button-secondary',
+            text: 'Zurück'
+        },
+        {
+            action() {
+                return this.next();
+            },
+            classes: 'shepherd-button-primary',
+            text: 'Weiter'
+        }
+    ],
+    id: 'creating'
+});
+
+tour.addStep({
+    title: 'Einstellungen',
+    text: 'Lassen Sie sich die unterschiedlichen Anlagentypen auf der Karte anzeigen.',
+    attachTo: {
+        element: '#js-map-layers-box',
+        on: 'left'
+    },
+    buttons: [
+        {
+            action() {
+                return this.back();
+            },
+            classes: 'shepherd-button-secondary',
+            text: 'Zurück'
+        },
+        {
+            action() {
+                return this.complete();
+            },
+            classes: 'shepherd-button-primary',
+            text: 'Fertig'
+        }
+    ],
+    id: 'creating'
+});
+
+tour.start();
