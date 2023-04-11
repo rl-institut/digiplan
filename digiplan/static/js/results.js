261005ef0dab7f5f0e62cdb10f7bf4cc659b27bb
finnus <finn@hees-web.de>
2023-04-11 14:24:39 +0200

    add popover in results view and js handling; harmonize with panel_1 form #214

--- digiplan/static/js/results.js
+++ digiplan/static/js/results.js
@@ -1,6 +1,8 @@
 
 const resultsDropdown = document.getElementById("result_views");
+const image_results = document.getElementById("info_tooltip_results");
 
 resultsDropdown.addEventListener("change", function() {
    PubSub.publish(mapEvent.CHOROPLETH_SELECTED, resultsDropdown.value);
+   image_results.title = resultsDropdown.options[resultsDropdown.selectedIndex].title;
 });
