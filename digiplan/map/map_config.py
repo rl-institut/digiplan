e8385b692574d42d10b742862fbe611e77c066a0
josihoppe <116898820+josihoppe@users.noreply.github.com>
2023-04-25 10:45:19 +0200

    changed colors to HEX values

--- digiplan/map/map_config.py
+++ digiplan/map/map_config.py
@@ -5,8 +5,8 @@ from django_mapengine import legend
 
 LEGEND = {
     _("Renewables"): [
-        legend.LegendLayer(_("Wind turbine"), "", layer_id="wind", color="blue"),
-        legend.LegendLayer(_("Roof-mounted PV"), "", layer_id="pvroof", color="yellow"),
+        legend.LegendLayer(_("Wind turbine"), "", layer_id="wind", color="#6A89CC"),
+        legend.LegendLayer(_("Roof-mounted PV"), "", layer_id="pvroof", color="#FFD660"),
         legend.LegendLayer(_("Outdoor PV"), "", layer_id="pvground", color="#F6B93B"),
         legend.LegendLayer(_("Hydro"), "", layer_id="hydro", color="#9CC4D9"),
         legend.LegendLayer(_("Biomass"), "", layer_id="biomass", color="#52C41A"),
