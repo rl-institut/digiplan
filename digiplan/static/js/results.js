e41007633836a771683a1f44ebaf6730ab1ab40b
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-05-03 14:26:29 +0200

    Trigger simulation run by clicking on results tab

--- digiplan/static/js/results.js
+++ digiplan/static/js/results.js
@@ -1,44 +1,26 @@
 
 const resultsDropdown = document.getElementById("result_views");
 const imageResults = document.getElementById("info_tooltip_results");
-const simulateBtn = document.getElementById("simulate_btn");
 
 // Setup
 
 // Disable settings form submit
 $('#settings').submit(false);
 
-$("#settings :input").change(function() {
-    PubSub.publish(eventTopics.SETTINGS_CHANGED);
-});
-
 resultsDropdown.addEventListener("change", function() {
     PubSub.publish(mapEvent.CHOROPLETH_SELECTED, resultsDropdown.value);
     imageResults.title = resultsDropdown.options[resultsDropdown.selectedIndex].title;
 });
 
-simulateBtn.addEventListener("click", function() {
-    PubSub.publish(eventTopics.SIMULATION_STARTED);
-});
-
 
 // Subscriptions
-
-PubSub.subscribe(eventTopics.SETTINGS_CHANGED, activateSimulateBtn);
-PubSub.subscribe(eventTopics.SIMULATION_STARTED, simulate);
+PubSub.subscribe(eventTopics.MENU_RESULTS_SELECTED, simulate);
 PubSub.subscribe(eventTopics.SIMULATION_FINISHED, showResults);
 
 
 // Subscriber Functions
 
-function activateSimulateBtn(msg) {
-    simulateBtn.disabled = false;
-    return logMessage(msg);
-}
-
 function simulate(msg) {
-    simulateBtn.disabled = true;
-
     const settings = document.getElementById("settings");
     const formData = new FormData(settings); // jshint ignore:line
     $.ajax({
