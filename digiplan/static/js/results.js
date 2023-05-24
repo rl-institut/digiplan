096c47b28da3fa97374215486d8be6db8ffa413d
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-05-24 14:41:58 +0200

    Integrate celery to use django-oemof

--- digiplan/static/js/results.js
+++ digiplan/static/js/results.js
@@ -3,6 +3,8 @@ const resultsDropdown = document.getElementById("result_views");
 const imageResults = document.getElementById("info_tooltip_results");
 const simulation_spinner = document.getElementById("simulation_spinner");
 
+const SIMULATION_CHECK_TIME = 5000;
+
 // Setup
 
 // Disable settings form submit
@@ -17,6 +19,7 @@ resultsDropdown.addEventListener("change", function() {
 // Subscriptions
 PubSub.subscribe(eventTopics.MENU_RESULTS_SELECTED, simulate);
 PubSub.subscribe(eventTopics.MENU_RESULTS_SELECTED, showSimulationSpinner);
+PubSub.subscribe(eventTopics.SIMULATION_STARTED, checkResultsPeriodically);
 PubSub.subscribe(eventTopics.SIMULATION_FINISHED, showResults);
 PubSub.subscribe(eventTopics.SIMULATION_FINISHED, hideSimulationSpinner);
 
@@ -26,6 +29,15 @@ PubSub.subscribe(eventTopics.SIMULATION_FINISHED, hideSimulationSpinner);
 function simulate(msg) {
     const settings = document.getElementById("settings");
     const formData = new FormData(settings); // jshint ignore:line
+    if (store.cold.task_id != null) {
+        $.ajax({
+            url : "/oemof/terminate",
+            type : "POST",
+            processData: false,
+            contentType: false,
+            data : {task_id: store.cold.task_id},
+        });
+    }
     $.ajax({
         url : "/oemof/simulate",
         type : "POST",
@@ -33,11 +45,33 @@ function simulate(msg) {
         contentType: false,
         data : formData,
         success : function(json) {
-            map_store.cold.state.simulation_id = json.simulation_id;
-            PubSub.publish(eventTopics.SIMULATION_FINISHED, json.simulation_id);
+            store.cold.task_id = json.task_id;
+            PubSub.publish(eventTopics.SIMULATION_STARTED);
         },
     });
     return logMessage(msg);
+}
+
+function checkResultsPeriodically(msg) {
+    setTimeout(checkResults, SIMULATION_CHECK_TIME);
+    return logMessage(msg);
+}
+
+function checkResults() {
+    $.ajax({
+        url: "/oemof/simulate",
+        type : "GET",
+        data : {task_id: store.cold.task_id},
+        success: function(json) {
+            if (json.simulation_id == null) {
+                setTimeout(checkResults, SIMULATION_CHECK_TIME);
+            } else {
+                store.cold.task_id = null;
+                map_store.cold.state.simulation_id = json.simulation_id;
+                PubSub.publish(eventTopics.SIMULATION_FINISHED);
+            }
+        }
+    });
 }
 
 function showResults(msg, simulation_id) {
