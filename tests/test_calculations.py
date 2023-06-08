75672380c27885022f8f14794174d2d92e2ce314
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-06-08 15:29:37 +0200

    Add simulation test base class

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -7,10 +7,23 @@ from django_oemof import simulation
 from digiplan.map import calculations
 
 
-class RenewableElectricityProductionTest(SimpleTestCase):
-    """Test renewable electricity production calculation."""
+class SimulationTest(SimpleTestCase):
+    """Base class for simulation tests."""
 
     databases = ("default",)  # Needed, as otherwise django complains about tests using "default" DB
+    parameters = {
+        "s_v_2": 100,
+        "s_v_3": 100,
+        "s_v_4": 100,
+        "s_w_1": 1000,
+        "s_pv_ff_1": 300,
+        "s_pv_d_1": 200,
+        "s_b_1": 1000,
+    }
+
+    def setUp(self) -> None:
+        """Starts/loads oemof simulation for given parameters."""
+        self.simulation_id = simulation.simulate_scenario("scenario_2045", self.parameters)
 
     def tearDown(self) -> None:  # noqa: D102 Needed to keep results in test DB
         pass
@@ -19,16 +32,13 @@ class RenewableElectricityProductionTest(SimpleTestCase):
     def tearDownClass(cls):  # noqa: D102,ANN102,ANN206 Needed to keep results in test DB
         pass
 
+
+class RenewableElectricityProductionTest(SimulationTest):
+    """Test renewable electricity production calculation."""
+
     def test_renewable_electricity_production(self):  # noqa: D102,ANN201
-        parameters = {
-            "s_v_2": 100,
-            "s_v_3": 100,
-            "s_v_4": 100,
-            "s_w_1": 1000,
-            "s_pv_ff_1": 300,
-            "s_pv_d_1": 200,
-            "s_b_1": 1000,
-        }
-        simulation_id = simulation.simulate_scenario("scenario_2045", parameters)
-        results = oemof_results.get_results(simulation_id, calculations=[calculations.renewable_electricity_production])
+        results = oemof_results.get_results(
+            self.simulation_id,
+            calculations=[calculations.renewable_electricity_production],
+        )
         assert list(results.values())[0].iloc[0] > 0
