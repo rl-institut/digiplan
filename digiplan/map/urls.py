31f2075dc889dfeabfe0674d3c860a58a48a5920
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-06-08 17:37:22 +0200

    Refactor choropleths to use new base class

--- digiplan/map/urls.py
+++ digiplan/map/urls.py
@@ -9,6 +9,6 @@ app_name = "map"
 
 urlpatterns = [
     path("", views.MapGLView.as_view(), name="map"),
-    path("choropleth/<str:lookup>/<str:scenario>", views.get_choropleth, name="choropleth"),
+    path("choropleth/<str:lookup>/<str:layer_id>", views.get_choropleth, name="choropleth"),
     path("popup/<str:lookup>/<int:region>", views.get_popup, name="popup"),
 ]
