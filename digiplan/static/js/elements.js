e48c31f6baf1ddfa72a564b3aa8226b52353f21b
Bryan Lancien <bryanlancien.ui@gmail.com>
2023-07-06 12:37:40 +0100

    add empty onboarding modal

--- digiplan/static/js/elements.js
+++ digiplan/static/js/elements.js
@@ -1,2 +1,8 @@
 export const statusquoDropdown = document.getElementById("situation_today");
 export const resultsTabs = document.getElementById("results-tabs");
+
+// Onboarding modal
+document.addEventListener('DOMContentLoaded', (event) => {
+    var myModal = new bootstrap.Modal(document.getElementById('onboardingModal'), {});
+    myModal.show();
+});
