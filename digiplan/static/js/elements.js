c616a20c15e0abfa0b8ebb8d5927688372cc0092
Bryan Lancien <bryanlancien.ui@gmail.com>
2023-07-11 15:13:41 +0100

    prevent continuous cycle of onboarding carousel

--- digiplan/static/js/elements.js
+++ digiplan/static/js/elements.js
@@ -1,8 +1,46 @@
 export const statusquoDropdown = document.getElementById("situation_today");
 export const resultsTabs = document.getElementById("results-tabs");
 
-// Onboarding modal
+// Show onboarding modal on start
 document.addEventListener('DOMContentLoaded', (event) => {
     var myModal = new bootstrap.Modal(document.getElementById('onboardingModal'), {});
     myModal.show();
 });
+
+// Prevent continuous cycle of modal carousel
+document.addEventListener("DOMContentLoaded", function() {
+    var carouselEl = document.querySelector('#carouselExampleIndicators');
+    var carousel = new bootstrap.Carousel(carouselEl, {
+      wrap: false
+    });
+  
+    var prevButton = document.querySelector('.carousel-control-prev');
+    var nextButton = document.querySelector('.carousel-control-next');
+  
+    prevButton.addEventListener('click', function (event) {
+      event.preventDefault();
+      carousel.prev();
+    });
+  
+    nextButton.addEventListener('click', function (event) {
+      event.preventDefault();
+      carousel.next();
+    });
+    
+    carouselEl.addEventListener('slid.bs.carousel', function () {
+      const carouselItems = carouselEl.querySelectorAll('.carousel-item');
+      const currentIndex = Array.prototype.indexOf.call(carouselItems, carouselEl.querySelector('.carousel-item.active'));
+      if (currentIndex === 0) {
+        prevButton.classList.add('transparent');
+      } else {
+        prevButton.classList.remove('transparent');
+      }
+  
+      if (currentIndex === carouselItems.length - 1) {
+        nextButton.classList.add('transparent');
+      } else {
+        nextButton.classList.remove('transparent');
+      }
+    });
+  });
+  
