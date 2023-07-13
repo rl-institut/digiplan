Fix energy and capacity units

digiplan/map/choropleths.py
@@ -95,28 +95,28 @@ class Energy2045Choropleth(Choropleth):  # noqa: D101
 class EnergyCapitaChoropleth(Choropleth):  # noqa: D101
     def get_values_per_feature(self) -> dict[int, float]:  # noqa: D102
         energies = calculations.energies_per_municipality()
-        energies_per_capita = calculations.calculate_capita_for_value(energies)
+        energies_per_capita = calculations.calculate_capita_for_value(energies) * 1e3
         return energies_per_capita.sum(axis=1).to_dict()
 
 
 class EnergyCapita2045Choropleth(Choropleth):  # noqa: D101
     def get_values_per_feature(self) -> dict[int, float]:  # noqa: D102
         energies = calculations.energies_per_municipality_2045(self.map_state["simulation_id"])
-        energies_per_capita = calculations.calculate_capita_for_value(energies)
+        energies_per_capita = calculations.calculate_capita_for_value(energies) * 1e3
         return energies_per_capita.sum(axis=1).to_dict()
 
 
 class EnergySquareChoropleth(Choropleth):  # noqa: D101
     def get_values_per_feature(self) -> dict[int, float]:  # noqa: D102
         energies = calculations.energies_per_municipality()
-        energies_per_square = calculations.calculate_square_for_value(energies)
+        energies_per_square = calculations.calculate_square_for_value(energies) * 1e3
         return energies_per_square.sum(axis=1).to_dict()
 
 
 class EnergySquare2045Choropleth(Choropleth):  # noqa: D101
     def get_values_per_feature(self) -> dict[int, float]:  # noqa: D102
         energies = calculations.energies_per_municipality_2045(self.map_state["simulation_id"])
-        energies_per_square = calculations.calculate_square_for_value(energies)
+        energies_per_square = calculations.calculate_square_for_value(energies) * 1e3
         return energies_per_square.sum(axis=1).to_dict()
 
 