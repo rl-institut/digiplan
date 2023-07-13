e4d96121ba17627ab5a7cd574bc522232c6cf209
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2023-07-13 13:32:22 +0200

    Release v0.5.0

--- digiplan/__init__.py
+++ digiplan/__init__.py
@@ -1,4 +1,4 @@
 """Digiplan init - holds current version."""
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace("-", ".", 1).split(".")])
