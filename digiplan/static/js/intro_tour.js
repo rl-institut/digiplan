2174ba66d975ca231f195979dddfb919c8e1973b
josihoppe <116898820+josihoppe@users.noreply.github.com>
2023-08-17 15:30:54 +0200

    added Tour-Steps for Situation today and Settings and some for results

--- digiplan/static/js/intro_tour.js
+++ digiplan/static/js/intro_tour.js
@@ -14,7 +14,7 @@ const tour = new Shepherd.Tour({  // jshint ignore:line
 
 tour.addStep({
     title: 'Navigation',
-    text: 'Schritt für Schritt zu Ihrem eigenen Szenario',
+    text: 'Schritt für Schritt zu Ihrem eigenen Szenario.',
     attachTo: {
         element: '.steps',
         on: 'bottom'
@@ -35,14 +35,14 @@ tour.addStep({
         }
 
     ],
-    id: 'creating'
+    id: 'start'
 });
 
 tour.addStep({
-    title: 'Einstellungen',
-    text: 'Schauen Sie sich die Situation heute an.',
+    title: 'Situation heute',
+    text: 'Schauen Sie sich die Situation heute an. Wählen Sie eine Kategorie aus.',
     attachTo: {
-        element: '#panel_1_today',
+        element: '#situation_today',
         on: 'right'
     },
     buttons: [
@@ -64,12 +64,94 @@ tour.addStep({
     id: 'situation_today'
 });
 
+
 tour.addStep({
-    title: 'Einstellungen',
+    title: 'Situation heute',
+    text: 'Zu jeder Kategorie gibt es ein Diagramm.',
+    attachTo: {
+        element: '#region_chart_statusquo',
+        on: 'right'
+    },
+    buttons: [
+        {
+            action() {
+                return this.back();
+            },
+            classes: 'shepherd-button-secondary',
+            text: 'Zurück'
+        },
+        {
+            action() {
+                return this.next();
+            },
+            classes: 'shepherd-button-primary',
+            text: 'Weiter'
+        }
+    ],
+    id: 'region_chart'
+});
+
+
+tour.addStep({
+    title: 'Karte',
     text: 'Lassen Sie sich die unterschiedlichen Anlagentypen auf der Karte anzeigen.',
     attachTo: {
         element: '#js-map-layers-box',
         on: 'left'
+    },
+    buttons: [
+        {
+            action() {
+                return this.back();
+            },
+            classes: 'shepherd-button-secondary',
+            text: 'Zurück'
+        },
+        {
+            action() {
+                return this.next();
+            },
+            classes: 'shepherd-button-primary',
+            text: 'Weiter'
+        }
+    ],
+    id: 'layer_switch'
+});
+
+
+tour.addStep({
+    title: 'Karte',
+    text: 'Klicken Sie auf ein einzelnes Icon, um mehr über dieses Kraftwerk etc. zu erfahren.',
+    attachTo: {
+        element: '.maplibregl-canvas',
+        on: 'top'
+    },
+    buttons: [
+        {
+            action() {
+                return this.back();
+            },
+            classes: 'shepherd-button-secondary',
+            text: 'Zurück'
+        },
+        {
+            action() {
+                return this.next();
+            },
+            classes: 'shepherd-button-primary',
+            text: 'Weiter'
+        }
+    ],
+    id: 'cluster_popup'
+});
+
+
+tour.addStep({
+    title: 'Nächster Schritt',
+    text: 'Hier gehts weiter zu den Einstellungen.',
+    attachTo: {
+        element: '#menu_next_btn',
+        on: 'bottom'
     },
     buttons: [
         {
@@ -86,29 +168,6 @@ tour.addStep({
             },
             classes: 'shepherd-button-primary',
             text: 'Weiter'
-        }
-    ],
-    id: 'layer_switch'
-});
-
-
-tour.addStep({
-    title: 'Nächster Schritt',
-    text: 'Hier gehts weiter. Klicken Sie, um zum nächsten Schritt zu kommen.',
-    attachTo: {
-        element: '#menu_next_btn',
-        on: 'bottom'
-    },advanceOn: {
-        selector: '#menu_next_btn',
-        event: 'click'
-    },
-    buttons: [
-        {
-            action() {
-                return this.back();
-            },
-            classes: 'shepherd-button-secondary',
-            text: 'Zurück'
         }
     ],
     id: 'menu_next_btn'
@@ -166,15 +225,13 @@ tour.addStep({
     id: 'more_slider'
 });
 
+
 tour.addStep({
-    title: 'Nächster Schritt',
-    text: 'Hier gehts weiter. Klicken Sie, um zum nächsten Schritt zu kommen.',
+    title: 'Einstellungen',
+    text: 'Schauen Sie, wie sich die Verteilung verändert.',
     attachTo: {
-        element: '#menu_next_btn',
-        on: 'bottom'
-    },advanceOn: {
-        selector: '#menu_next_btn',
-        event: 'click'
+        element: '.power-mix__chart',
+        on: 'right'
     },
     buttons: [
         {
@@ -183,16 +240,160 @@ tour.addStep({
             },
             classes: 'shepherd-button-secondary',
             text: 'Zurück'
+        },
+        {
+            action() {
+                return this.next();
+            },
+            classes: 'shepherd-button-primary',
+            text: 'Weiter'
+        }
+    ],
+    id: 'power_mix_chart'
+});
+
+
+tour.addStep({
+    title: 'Einstellungen',
+    text: 'Wechseln Sie zu den Einstellungen für Wärme und Infrastruktur.',
+    attachTo: {
+        element: '#settings_area_tab',
+        on: 'right'
+    },
+    buttons: [
+        {
+            action() {
+                return this.back();
+            },
+            classes: 'shepherd-button-secondary',
+            text: 'Zurück'
+        },
+        {
+            action() {
+                return this.next();
+            },
+            classes: 'shepherd-button-primary',
+            text: 'Weiter'
+        }
+    ],
+    id: 'settings_area_tab'
+});
+
+
+tour.addStep({
+    title: 'Nächster Schritt',
+    text: 'Hier gehts weiter zu den Ergebnissen und Sie starten auch gleich die Simulierung ihres Szenarios.',
+    attachTo: {
+        element: '#menu_next_btn',
+        on: 'bottom'
+    },
+    buttons: [
+        {
+            action() {
+                return this.back();
+            },
+            classes: 'shepherd-button-secondary',
+            text: 'Zurück'
+        },
+        {
+            action() {
+                document.getElementById("menu_next_btn").click();
+                return this.next();
+            },
+            classes: 'shepherd-button-primary',
+            text: 'Weiter'
         }
     ],
     id: 'menu_next_btn2'
 });
 
 tour.addStep({
-    title: 'Einstellungen',
-    text: 'Schauen Sie sich verschiedene Daten an.',
+    title: 'Ergebnisse',
+    text: 'Schauen Sie sich die Ergebnisse der Simulation auf der Karte an. Wählen Sie eine Kategorie aus.',
     attachTo: {
         element: '#panel_3_results',
+        on: 'right'
+    },
+    buttons: [
+        {
+            action() {
+                return this.back();
+            },
+            classes: 'shepherd-button-secondary',
+            text: 'Zurück'
+        },
+        {
+            action() {
+                return this.next();
+            },
+            classes: 'shepherd-button-primary',
+            text: 'Weiter'
+        }
+    ],
+    id: 'results_on_map'
+});
+
+
+tour.addStep({
+    title: 'Ergebnisse',
+    text: 'Wählen Sie auf der Karte eine Region aus und schauen Sie sich die detailierten Informationen in einem Diagramm an.',
+    attachTo: {
+        element: '.maplibregl-canvas',
+        on: 'top'
+    },
+    buttons: [
+        {
+            action() {
+                return this.back();
+            },
+            classes: 'shepherd-button-secondary',
+            text: 'Zurück'
+        },
+        {
+            action() {
+                return this.next();
+            },
+            classes: 'shepherd-button-primary',
+            text: 'Weiter'
+        }
+    ],
+    id: 'popups'
+});
+
+
+tour.addStep({
+    title: 'Einstellungen',
+    text: 'Wechseln Sie von der Karten- zur Diagramm-Ansicht, sobald die Simulation abgeschlossen ist.',
+    attachTo: {
+        element: '#chart-view-tab',
+        on: 'bottom'
+    },
+    buttons: [
+        {
+            action() {
+                return this.back();
+            },
+            classes: 'shepherd-button-secondary',
+            text: 'Zurück'
+        },
+        {
+            action() {
+                document.getElementById("chart_view_tab").click();
+                return this.next();
+            },
+            classes: 'shepherd-button-primary',
+            text: 'Weiter'
+        }
+    ],
+    id: 'chart_view_tab'
+});
+
+
+tour.addStep({
+    title: 'Fertig',
+    text: 'Viel Spaß mit dem Tool :D.',
+    attachTo: {
+        element: '#chart_view_tab',
         on: 'right'
     },
     buttons: [
@@ -211,8 +412,9 @@ tour.addStep({
             text: 'Fertig'
         }
     ],
-    id: 'results'
+    id: 'end'
 });
+
 
 onbaordingCloseBtn.addEventListener("click", function() {
   tour.start();
