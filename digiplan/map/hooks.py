Fix calculation of central HP share: use all sectors

digiplan/map/hooks.py
@@ -184,11 +184,8 @@ def adapt_heat_settings(scenario: str, data: dict, request: HttpRequest) -> dict
                 hp_share = data.pop(hp_sliders[sector]) / 100
                 hp_energy[sector] = demand[sector] * hp_share
             else:
-                if sector == "hh":  # noqa: PLR5501
-                    hp_share = data.pop("w_z_wp_1") / 100
-                    hp_energy[sector] = demand[sector] * hp_share
-                else:
-                    hp_energy[sector] = demand[sector] * 0
+                hp_share = data["w_z_wp_1"] / 100
+                hp_energy[sector] = demand[sector] * hp_share
 
         # HP Capacity and Energies
         hp_energy_total = pd.concat(hp_energy.values(), axis=1).sum(axis=1)